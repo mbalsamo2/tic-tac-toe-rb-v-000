b9f16d956f1ae7d022ca1cfb68d95707ea1d4a56
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-10 17:57:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ def valid_move?(board, index)
 end
 
 # completes one full turn
-def turn(board, index)
+def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
