c2821c6b16538e0eed64243637cb7afbe869c668
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-10 17:28:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,6 +27,10 @@ def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
@@ -44,20 +48,25 @@ def turn(board)
   end
 end
 
-# plays/loops through game 9 times
-def play(board)
-turn = 0
-  until turn == 9
-    turn(board)
-    turn += 1
+# determine how many turns have been made
+def turn_count(board)
+counter = 0
+board.each do |turn|
+  if turn == "X" || turn == "O"
+      counter += 1
+  end
+ end
+ return counter
+end
+
+# determine if it's X's or O's turn
+def current_player(board)
+  if turn_count(board) % 2 == 0
+       "X"
+  elsif turn_count(board) % 2 == 1
+       "O"
   end
 end
-
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
 
 # false for no win/draw, returns winning array
 def won?(board)
@@ -90,23 +99,11 @@ def winner(board)
  end
 end
 
-# determine how many turns have been made
-def turn_count(board)
-counter = 0
-board.each do |turn|
-  if turn == "X" || turn == "O"
-      counter += 1
-  end
- end
- return counter
-end
-
-
-# determine if it's X's or O's turn
-def current_player(board)
-  if turn_count(board) % 2 == 0
-       "X"
-  elsif turn_count(board) % 2 == 1
-       "O"
+# plays/loops through game 9 times
+def play(board)
+turn = 0
+  until turn == 9
+    turn(board)
+    turn += 1
   end
 end
