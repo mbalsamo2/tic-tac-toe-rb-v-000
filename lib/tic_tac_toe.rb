fad8e88934d7bfff6d869fcc90fe1bfaccdd68b9
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-10 17:30:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ WIN_COMBINATIONS = [
   [2,4,6] # diagonal R to L
 ]
 
-
+# outputs current board representation
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
