96316d8c32f863491713f35c0137edad47a02f9e
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-10 17:25:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,16 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left collumn
+  [1,4,7], # middle collumn
+  [2,5,8], # right collumn
+  [0,4,8], # diagonal L to R
+  [2,4,6] # diagonal R to L
+]
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -45,17 +58,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2], # top row
-  [3,4,5], # middle row
-  [6,7,8], # bottom row
-  [0,3,6], # left collumn
-  [1,4,7], # middle collumn
-  [2,5,8], # right collumn
-  [0,4,8], # diagonal L to R
-  [2,4,6] # diagonal R to L
-]
 
 # false for no win/draw, returns winning array
 def won?(board)
