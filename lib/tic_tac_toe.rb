e9ec4b39e03c0b26971aa54afee258339bab43b2
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-10 17:19:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,3 +46,25 @@ def winner(board)
    board[winning_combo.first]
  end
 end
+
+# determine how many turns have been made
+def turn_count(board)
+counter = 0
+board.each do |turn|
+  if turn == "X" || turn == "O"
+      counter += 1
+  end
+ end
+ return counter
+end
+
+
+# determine if it's X's or O's turn
+def current_player(board)
+  if turn_count(board) % 2 == 0
+       "X"
+  elsif turn_count(board) % 2 == 1
+       "O"
+  end
+end
+
